38964eaf562d00a1200dc26ed8f357ba27be9b19
Sebastian Gug <sebastiangug@gmail.com>
2020-12-28 23:23:02 +0000

    secrets & small config

--- src/modules/config/services/config/config.service.ts
+++ src/modules/config/services/config/config.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import * as fs from "fs";
+
+export enum Environments {
+  Production = "PRODUCTION",
+  Development = "DEVELOPMENT",
+  STAGING = "STAGING",
+}
 
 @Injectable()
 export class ConfigService {
+  public environment(): Environments {
+    return (
+      (process.env.ENVIRONMENT as Environments) ?? Environments.Development
+    );
+  }
+
   public get_backend_root_url(): string {
     return "";
   }
